d654d88d93eb78c67b192d4f210da0063662cb1f
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2023-05-10 18:02:47 -0300

    Design changes

--- script.js
+++ script.js
@@ -30,14 +30,26 @@ var preguntas = [
 ];
 
 // Variables globales
-var tarjetaActual = null;
 var preguntasRestantes = preguntas.slice();
+var tarjetaActual = null;
 
-// Función para mostrar una tarjeta aleatoria
-function mostrarTarjetaAleatoria() {
-  
+// Función para mostrar la tarjeta de pregunta o la tarjeta de fin
+function mostrarTarjeta() {
+  var contenedorTarjetas = document.getElementById("card-container");
+  contenedorTarjetas.innerHTML = "";
+
   if (preguntasRestantes.length === 0) {
-    preguntasRestantes = preguntas.slice();
+    mostrarTarjetaFin();
+  } else {
+    mostrarTarjetaAleatoria();
+  }
+}
+
+// Función para mostrar una tarjeta de pregunta aleatoria
+function mostrarTarjetaAleatoria() {
+  if (preguntasRestantes.length === 0) {
+    mostrarTarjetaFin();
+    return;
   }
 
   var indicePregunta = Math.floor(Math.random() * preguntasRestantes.length);
@@ -64,11 +76,32 @@ function mostrarTarjetaAleatoria() {
   tarjeta.appendChild(tarjetaBody);
 
   var contenedorTarjetas = document.getElementById("card-container");
-  contenedorTarjetas.innerHTML = "";
   contenedorTarjetas.appendChild(tarjeta);
 
   tarjetaActual = tarjeta;
+}
 
+// Func para mostrar la tarjeta de fin
+function mostrarTarjetaFin() {
+  var tarjetaFin = document.createElement("div");
+  tarjetaFin.classList.add("card", "text-center", "mb-4");
+  var tarjetaBody = document.createElement("div");
+  tarjetaBody.classList.add("card-body");
+  var tarjetaTitulo = document.createElement("h5");
+  tarjetaTitulo.classList.add("card-title");
+  tarjetaTitulo.textContent = "FIN";
+  tarjetaBody.appendChild(tarjetaTitulo);
+  var botonReiniciar = document.createElement("button");
+  botonReiniciar.classList.add("btn", "btn-success");
+  botonReiniciar.textContent = "Reiniciar";
+  botonReiniciar.addEventListener("click", reiniciarPreguntas);
+  tarjetaBody.appendChild(botonReiniciar);
+  tarjetaFin.appendChild(tarjetaBody);
+
+  var contenedorTarjetas = document.getElementById("card-container");
+  contenedorTarjetas.appendChild(tarjetaFin);
+
+  tarjetaActual = null;
 }
 
 // Func para manejar la respuesta seleccionada
@@ -76,42 +109,37 @@ function manejarRespuesta(evento) {
   var botonesRespuesta = tarjetaActual.querySelectorAll("button");
   botonesRespuesta.forEach(function(boton) {
     boton.disabled = true;
+    if (boton.dataset.correcta !== "true") {
+      boton.classList.add("bg-light");
+    }
   });
 
   var respuestaSeleccionada = evento.target.dataset.correcta === "true";
   if (respuestaSeleccionada) {
     tarjetaActual.classList.add("bg-success");
     document.getElementById("result").textContent = "👍🏻";
-
-    // Agregar la clase .bg-secondary a los botones de respuesta incorrecta
-    botonesRespuesta.forEach(function(boton) {
-      if (boton.dataset.correcta !== "true") {
-        boton.classList.add("bg-secondary");
-      }
-    });
   } else {
     tarjetaActual.classList.add("bg-danger");
     document.getElementById("result").textContent = "Respuesta incorrecta 😬";
-
-    // Agregar la clase .bg-secondary a todos los botones de respuesta incorrecta
-    botonesRespuesta.forEach(function(boton) {
-      if (boton.dataset.correcta !== "true") {
-        boton.classList.add("bg-secondary");
-      }
-    });
   }
 
   setTimeout(function() {
     document.getElementById("result").textContent = "";
-  }, 1600);
+  }, 1800);
 
-  // Temporizador de 2 segundos para mostrar la siguiente pregunta
+  // Temporizador de 2 segundos para mostrar la siguiente pregunta o la tarjeta de fin
   setTimeout(function() {
-    mostrarTarjetaAleatoria();
-  }, 1600);
+    mostrarTarjeta();
+  }, 1800);
+}
+
+// Func para reiniciar las preguntas
+function reiniciarPreguntas() {
+  preguntasRestantes = preguntas.slice();
+  mostrarTarjeta();
 }
 
 // Llamada a la función para mostrar la primera tarjeta al cargar la página
 window.onload = function() {
-  mostrarTarjetaAleatoria();
+  mostrarTarjeta();
 };
